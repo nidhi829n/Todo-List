2423f1ec20a0ba50b919b375c58df43cfb9f22e4
Nidhi Mishra <151422993+nidhi829n@users.noreply.github.com>
2025-07-28 10:42:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let btn= document.querySelector("button");
+let ul= document.querySelector("ul");
+let inp= document.querySelector("input");
+
+btn.addEventListener("click", function(){
+    let item= document.createElement("li");
+    item.innerText=inp.value;
+
+    let delbtn= document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    inp.value="";
+});
+
+// let delBtns= document.querySelectorAll(".delete");
+
+// for(delbt of delBtns){
+//     delbt.addEventListener("click", function(){
+//         let par=this.parentElement;
+//         par.remove();
+//     });
+// }
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listitm= event.target.parentElement;
+        listitm.remove();
+    }
+
+});
